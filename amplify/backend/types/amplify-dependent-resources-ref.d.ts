8929be3fbd1d3231f32758b499f58cba36a10bc5
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2023-10-23 18:20:21 -0300

    Amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "galeriaimagens8c43c22b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3e7d60aa3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
